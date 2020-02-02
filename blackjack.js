c0cf2869f5622ed1691744f74eaf400547c1f549
KB-Z <54561741+KB-Z@users.noreply.github.com>
2020-02-02 13:25:01 -0500

    Fixed CSS issue
    Fixed issue with CSS after making some tweaking that broke it.

--- blackjack.js
+++ blackjack.js
@@ -1,4 +1,4 @@
-const cardSuits = ["Spades", "Clubs", "Diamons", "Hearts"];
+const cardSuits = ["Spades", "Clubs", "Diamonds", "Hearts"];
 const cardValues = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
 let cardDeck = new Array();
 let players = new Array();
