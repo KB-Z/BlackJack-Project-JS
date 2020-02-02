50df89ae815055a4507cc02195756ba45831cf9e
KB-Z <54561741+KB-Z@users.noreply.github.com>
2020-02-02 09:57:15 -0500

    creating values for named cards

--- Deck.js
+++ Deck.js
@@ -1,3 +1,17 @@
-var cardSuits = ["Spades", "Clubs", "Diamons", "Hearts"];
-var cardValues = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
-var cardDeck = new Array();
+const cardSuits = ["Spades", "Clubs", "Diamons", "Hearts"];
+const cardValues = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
+let cardDeck = new Array();
+
+const createDeck = () => {
+    cardDeck = new Array();
+    for (let i = 0; i < cardValues.length; i++) {
+        for (let x = 0; x < cardSuits.length; x++) {
+            let weight = parseInt(cardValues[i]);
+            if (cardValues[i] == "J" || cardValues[i] == "Q" || cardValues[i] == "K") {
+                weight = 10;
+            }
+            
+        }
+        
+    }
+    }
